b38d901e2218106adb5324d114f5f6425e31ed99
James Hoofard <james.hoofard.iii@gmail.com>
2021-10-26 11:28:48 -0500

    initial commit

---
+++ src/utils/initialData.js
@@ -0,0 +1,11 @@
+const initialData = {
+    monsters: {
+        lich: {health: 200, initiative: 22},
+        skeleton: {health: 20, initiative: 12}
+    },
+    players: {
+        redLightningwingXIII: {health: 55, initiative: 35},
+        sensei: {health: 60, initiative: 10},
+        darkNechran: {health: 40, initiative: 18}
+    }
+}
