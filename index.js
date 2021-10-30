ad61eea2f1803473aa52c8d43f5a8327bf4d3406
James Hoofard <james.hoofard.iii@gmail.com>
2021-10-30 13:10:56 -0500

    reworked app to use vanilla js and html, got base idea working, now on to more complex stuff

---
+++ index.js
@@ -0,0 +1,22 @@
+let initInput = document.querySelector('.initInput')
+let healthInput = document.querySelector('.healthInput')
+let nameInput = document.querySelector('.nameInput')
+let submitButton = document.querySelector('.submitButton')
+
+
+function createHTMLEl(event){
+    event.preventDefault()
+    let containter = document.querySelector('.container')
+    let initBox = document.createElement('p')
+    let healthBox = document.createElement('p')
+    let nameBox = document.createElement('p')
+    initBox.innerText = `Initiative: ${initInput.value}`
+    healthBox.innerText = `Health: ${healthInput.value}`
+    nameBox.innerText = `Name: ${nameInput.value} `
+    containter.append(initBox, healthBox, nameBox)
+    console.log(healthInput.value, nameInput.value, initInput.value)
+    console.log(initBox, healthBox, nameBox)
+}
+
+document
+.addEventListener('submit', createHTMLEl)
